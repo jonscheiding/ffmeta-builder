04190d6b4a5546c062a4970c2f1a506d1ee9ae28
Jon Scheiding <jonscheiding@gmail.com>
2021-01-16 12:53:44 -0500

    Add generation for title & author

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 import fs from 'fs';
+import mustache from 'mustache';
 
+/**
+ * The arguments to the index function.
+ * @typedef {Object} Arguments 
+ * @property {string} chapters
+ * @property {string} title
+ * @property {string} author
+ */
+
+ /**
+  * Generates an FFMETA structure from the provided arguments.
+  * @param {Arguments} argv 
+  */
 export default function index(argv) {
-  console.log(argv);
+  const template = fs.readFileSync(`${__dirname}/ffmeta.mustache`);
+  
+  const tags = [];
+  for(const key of ['title', 'author']) {
+    if(key in argv) {
+      tags.push({key, value: argv[key]});
+    }
+  }
+
+  const metadata = { tags };
+  
+  console.log(mustache.render(template.toString(), metadata));
 }
